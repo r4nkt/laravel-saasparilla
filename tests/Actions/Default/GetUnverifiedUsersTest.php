b6e56e14a02a9449762e2579116dc72c81e6e9a3
Travis Elkins <travis.elkins+github@gmail.com>
2021-04-21 11:48:49 +0200

    WIP: Cleaned up tests, deleting some and updating/moving others.

--- tests/Actions/Default/GetUnverifiedUsersTest.php
+++ tests/Actions/Default/GetUnverifiedUsersTest.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace R4nkt\Saasparilla\Tests\Actions\Users;
+namespace R4nkt\Saasparilla\Tests\Actions\Default;
 
 use Carbon\Carbon;
-use R4nkt\Saasparilla\Contexts\Context;
+use R4nkt\Saasparilla\Actions\Contracts\GetsResources;
+use R4nkt\Saasparilla\GetterFactory;
 use R4nkt\Saasparilla\Saasparilla;
 use R4nkt\Saasparilla\Tests\TestCase;
 use R4nkt\Saasparilla\Tests\TestClasses\User;
 
-class FindInactiveUsersTest extends TestCase
+class GetUnverifiedUsersTest extends TestCase
 {
-    protected Context $context;
+    protected GetsResources $getter;
     protected int $threshold = 20;
 
     public function setUp(): void
@@ -19,10 +20,10 @@ class FindInactiveUsersTest extends TestCase
 
         $this->travelTo(Carbon::create('2020-01-01 00:00:00'));
 
-        config(['saasparilla.inactive_contexts.users.model' => User::class]);
-        config(['saasparilla.inactive_contexts.users.actions.find.params.threshold' => $this->threshold]);
+        config(['saasparilla.getters.unverified-users.params.model' => User::class]);
+        config(['saasparilla.getters.unverified-users.params.threshold' => $this->threshold]);
 
-        $this->context = app(Saasparilla::class)->inactiveContext('users');
+        $this->getter = GetterFactory::make('unverified-users');
     }
 
     /** @test */
@@ -30,7 +31,7 @@ class FindInactiveUsersTest extends TestCase
     {
         $newUser = User::factory()->unverified()->create();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
     }
 
     /** @test */
@@ -38,11 +39,11 @@ class FindInactiveUsersTest extends TestCase
     {
         $newUser = User::factory()->unverified()->create();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
 
         $this->travel($this->threshold)->days();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
     }
 
     /** @test */
@@ -50,11 +51,12 @@ class FindInactiveUsersTest extends TestCase
     {
         $newUser = User::factory()->unverified()->create();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
 
-        $this->travel($this->threshold + 1)->days();
+        $this->travel($this->threshold)->days();
+        $this->travel(1)->seconds();
 
-        $this->assertCount(1, $this->context->find());
+        $this->assertCount(1, $this->getter->get());
     }
 
     /** @test */
@@ -62,10 +64,10 @@ class FindInactiveUsersTest extends TestCase
     {
         $newUser = User::factory()->create();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
 
         $this->travel($this->threshold + 1)->days();
 
-        $this->assertCount(0, $this->context->find());
+        $this->assertCount(0, $this->getter->get());
     }
 }
