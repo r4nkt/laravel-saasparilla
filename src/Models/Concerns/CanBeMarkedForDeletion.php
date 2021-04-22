cc40c2d23b4665be963d63b7aee8bb7e411cc88b
Travis Elkins <travis.elkins+github@gmail.com>
2021-04-22 17:07:12 +0200

    WIP: Fixed unmark-user-on-email-verified wiring.

--- src/Models/Concerns/CanBeMarkedForDeletion.php
+++ src/Models/Concerns/CanBeMarkedForDeletion.php
@@ -4,20 +4,18 @@ namespace R4nkt\Saasparilla\Models\Concerns;
 
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Event;
-use R4nkt\Saasparilla\Concerns\UsesSaasparillaConfig;
 use R4nkt\Saasparilla\Features;
+use R4nkt\Saasparilla\UnmarkerFactory;
 
 trait CanBeMarkedForDeletion
 {
-    use UsesSaasparillaConfig;
-
     protected static function bootCanBeMarkedForDeletion()
     {
         if (Features::hasMarksUnverifiedUsersForDeletionFeature()) {
             Event::listen(function (Verified $event) {
                 $options = Features::options(Features::marksUnverifiedUsersForDeletion());
 
-                $unmarker = self::unmarker($options['unmarker']);
+                $unmarker = UnmarkerFactory::make($options['params']['unmarker']);
 
                 $unmarker->unmark($event->user);
             });
