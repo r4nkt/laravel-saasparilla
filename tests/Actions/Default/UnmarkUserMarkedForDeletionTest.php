5e8427606110dc843d09a83e4b40d554f433dc4a
Travis Elkins <travis.elkins+github@gmail.com>
2021-04-29 16:52:51 +0200

    WIP: Updated to latest resource tidier fixes, and added test for unmarking users when verified.

--- tests/Actions/Default/UnmarkUserMarkedForDeletionTest.php
+++ tests/Actions/Default/UnmarkUserMarkedForDeletionTest.php
@@ -2,13 +2,14 @@
 
 namespace R4nkt\Saasparilla\Tests\Actions\Default;
 
+use Illuminate\Auth\Events\Verified;
 use R4nkt\Saasparilla\Tests\TestCase;
 use R4nkt\Saasparilla\Tests\TestClasses\User;
 
 class UnmarkUserMarkedForDeletionTest extends TestCase
 {
     /** @test */
-    public function it_marks_a_user()
+    public function it_unmarks_a_user()
     {
         $user = User::factory()->create();
 
@@ -18,6 +19,22 @@ class UnmarkUserMarkedForDeletionTest extends TestCase
         $this->assertNotNull($user->automatically_delete_at);
 
         $this->tidier->unmarker()->unmark($user);
+
+        $this->assertNull($user->deleting_soon_mail_sent_at);
+        $this->assertNull($user->automatically_delete_at);
+    }
+
+    /** @test */
+    public function it_unmarks_a_user_when_verified()
+    {
+        $user = User::factory()->create();
+
+        $this->tidier->culler()->marker()->mark($user);
+
+        $this->assertNotNull($user->deleting_soon_mail_sent_at);
+        $this->assertNotNull($user->automatically_delete_at);
+
+        event(new Verified($user));
 
         $this->assertNull($user->deleting_soon_mail_sent_at);
         $this->assertNull($user->automatically_delete_at);
